a8dcdab20c44a97b78ef080e3ef8448081a53a92
Hyedryn <quentin124@hotmail.be>
2023-01-06 10:05:11 -0500

    Add sift to connectivity matrix

--- Tractography/connectivityMatrixFromTractogram.py
+++ Tractography/connectivityMatrixFromTractogram.py
@@ -7,7 +7,10 @@ from dipy.io.image import load_nifti, save_nifti
 import dipy.tracking
 from dipy.io.streamline import load_tractogram
 
-def connectivityMatrix(folder_path, p, label_fname):
+def connectivityMatrix(folder_path, p, label_fname, input="TCKGEN"):
+
+    assert input in ["TCKGEN", "SIFT", "SIFT2"], "input must be either TCKGEN, SIFT or SIFT2"
+    
     odf_wmfod = folder_path + '/subjects/' + p + "/dMRI/ODF/MSMT-CSD/" + p + "_MSMT-CSD_WM_ODF.nii.gz"
     odf_csd_mrtrix = folder_path + '/subjects/' + p + "/dMRI/ODF/CSD/" + p + "_CSD_SH_ODF_mrtrix.nii.gz"
     if os.path.exists(odf_wmfod):
@@ -24,7 +27,16 @@ def connectivityMatrix(folder_path, p, label_fname):
     labels = np.round(labels_nii.get_fdata()).astype(int)
 
     tracking_path = folder_path + '/subjects/' + p + "/dMRI/tractography/"
-    tractogram_path = tracking_path + p + '_tractogram.trk'
+    if input == "TCKGEN":
+        tractogram_path = tracking_path + p + '_tractogram.trk'
+    elif input == "SIFT":
+        tractogram_path = tracking_path + p + '_tractogram_sift.tck'
+    elif input == "SIFT2":
+        raise Exception("SIFT2 not implemented yet")
+        pass # TODO
+    else:
+        raise Exception("Invalid input type")
+
     if os.path.exists(tractogram_path):
         tractogram = load_tractogram(tractogram_path, "same")
     else:
@@ -36,11 +48,13 @@ def connectivityMatrix(folder_path, p, label_fname):
                                              return_mapping=True,
                                              mapping_as_streamlines=True)
 
-    MV[:3, :] = 0
-    MV[:, :3] = 0
+    MV = np.delete(MV, 0, 0)
+    MV = np.delete(MV, 0, 1)
 
 
     import matplotlib.pyplot as plt
-    np.save(tracking_path + p + "_connectivity_matrix.npy", MV)
     plt.imshow(np.log1p(MV), interpolation='nearest')
-    plt.savefig(tracking_path + "connectivity.png")
+
+    if input == "SIFT" or input == "SIFT2":
+        np.save(tracking_path + p + "_connectivity_matrix_" + input + ".npy", MV)
+        plt.savefig(tracking_path + "connectivity" + input + ".png")
