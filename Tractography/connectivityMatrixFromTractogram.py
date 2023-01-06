4020a875225f6c6d20a5eb33af06e69210714b8d
Hyedryn <quentin124@hotmail.be>
2023-01-06 12:04:29 -0500

    Fix sift tractogram extension

--- Tractography/connectivityMatrixFromTractogram.py
+++ Tractography/connectivityMatrixFromTractogram.py
@@ -10,7 +10,7 @@ from dipy.io.streamline import load_tractogram
 def connectivityMatrix(folder_path, p, label_fname, input="TCKGEN"):
 
     assert input in ["TCKGEN", "SIFT", "SIFT2"], "input must be either TCKGEN, SIFT or SIFT2"
-    
+
     odf_wmfod = folder_path + '/subjects/' + p + "/dMRI/ODF/MSMT-CSD/" + p + "_MSMT-CSD_WM_ODF.nii.gz"
     odf_csd_mrtrix = folder_path + '/subjects/' + p + "/dMRI/ODF/CSD/" + p + "_CSD_SH_ODF_mrtrix.nii.gz"
     if os.path.exists(odf_wmfod):
@@ -30,7 +30,7 @@ def connectivityMatrix(folder_path, p, label_fname, input="TCKGEN"):
     if input == "TCKGEN":
         tractogram_path = tracking_path + p + '_tractogram.trk'
     elif input == "SIFT":
-        tractogram_path = tracking_path + p + '_tractogram_sift.tck'
+        tractogram_path = tracking_path + p + '_tractogram_sift.trk'
     elif input == "SIFT2":
         raise Exception("SIFT2 not implemented yet")
         pass # TODO
