ccf78a958c8d53363061a286333dd2faa66d289e
qdessain <quentin.dessain@uclouvain.be>
2024-04-10 11:59:00 +0200

    Added tractogram_filename and tck support to connectivity matrix

--- Tractography/connectivityMatrixFromTractogram.py
+++ Tractography/connectivityMatrixFromTractogram.py
@@ -9,9 +9,11 @@ from dipy.io.streamline import load_tractogram
 from scipy.ndimage import binary_dilation
 from skimage.morphology import ball
 
-def connectivityMatrix(folder_path, p, label_fname, input="TCKGEN", inclusive=False, dilation_radius=0, longitudinal=False):
+def connectivityMatrix(folder_path, p, label_fname, input="TCKGEN", inclusive=False, dilation_radius=0, longitudinal=False, tractogram_filename="tractogram"):
 
     assert input in ["TCKGEN", "SIFT", "SIFT2"], "input must be either TCKGEN, SIFT or SIFT2"
+
+    dwi_path = folder_path + '/subjects/' + p + '/dMRI/preproc/' + p + '_dmri_preproc.nii.gz'
 
     odf_wmfod = folder_path + '/subjects/' + p + "/dMRI/ODF/MSMT-CSD/" + p + "_MSMT-CSD_WM_ODF.nii.gz"
     odf_csd_mrtrix = folder_path + '/subjects/' + p + "/dMRI/ODF/CSD/" + p + "_CSD_SH_ODF_mrtrix.nii.gz"
@@ -63,9 +65,9 @@ def connectivityMatrix(folder_path, p, label_fname, input="TCKGEN", inclusive=Fa
 
     
     if input == "TCKGEN":
-        tractogram_path = tracking_path + p + '_tractogram.tck'
+        tractogram_path = tracking_path + p + f'_{tractogram_filename}.tck'
     elif input == "SIFT":
-        tractogram_path = tracking_path + p + '_tractogram_sift.tck'
+        tractogram_path = tracking_path + p + f'_{tractogram_filename}_sift.tck'
     elif input == "SIFT2":
         raise Exception("SIFT2 not implemented yet")
         pass # TODO
@@ -73,7 +75,7 @@ def connectivityMatrix(folder_path, p, label_fname, input="TCKGEN", inclusive=Fa
         raise Exception("Invalid input type")
 
     if os.path.exists(tractogram_path):
-        tractogram = load_tractogram(tractogram_path, "same")
+        tractogram = load_tractogram(tractogram_path, dwi_path)
     else:
         raise Exception("No tractogram found in " + tracking_path)
         return
